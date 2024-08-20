d92552f0b0773dc54b11d3573f96954cc4091531
lovelyyoshino <mpl9725440@gmail.com>
2024-08-20 14:29:31 +0800

    Update faiss

--- install.sh
+++ install.sh
@@ -179,9 +179,9 @@ sudo apt-get install ros-noetic-pcl-ros
 sudo apt-get install ros-noetic-cv-bridge
 
 # 安装faiss
-#git clone https://github.com/facebookresearch/faiss.git
-#cd faiss
-#mkdir build && cd build
-#cmake ..  -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
-#make -j6 && make install
-#cd ../../
+# git clone https://github.com/facebookresearch/faiss.git
+# cd faiss
+# mkdir build && cd build
+# cmake ..  -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
+# make -j6 && sudo make install
+# cd ../../
